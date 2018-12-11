3470927c424a0991f86965d5bc6b50924da0980e
Cristhian Márquez <cthan.mb@gmail.com>
2018-12-11 01:08:31 -0300

    cambio localhost por ip local

--- routes/bd.js
+++ routes/bd.js
@@ -1,7 +1,7 @@
 var mysql=require('mysql');
 
 var connection=mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
     password : 'Gomitrof456',
     port : 3306, //port mysql
