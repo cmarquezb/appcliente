30ea24278e782f119456b23928d079faf3e13f57
Cristhian Márquez <cthan.mb@gmail.com>
2018-12-12 15:41:48 -0300

    Filtros idOT autocomplado

--- routes/ajax/ajaxMostrarOt.js
+++ routes/ajax/ajaxMostrarOt.js
@@ -9,8 +9,12 @@ router.get('/', function(req, res, next) {
         //var sql = "SELECT idOT AS value, idOT AS label, idOT AS value1, idResponsables AS value2, idArea AS value3, Estados_idEstados AS value4, Equipos_idEquipos AS value5, Servicios_idServicios AS value6, Centro_idCentro AS value7, Clientes_idClientes AS value8, Fecha AS value9, HES AS value10, Valor AS value11, Observacion AS value12 FROM ot WHERE CAST(`idOT` AS CHAR)  LIKE '" +  cod_OT + "%' order by  idOT ASC";
         var sql = "SELECT ot.idOT AS value1, ot.idOT AS label,";
             sql +=" ot.idResponsables AS value2, responsables.Nombres AS value22,";
-            sql +=" ot.idArea AS value3, ot.Estados_idEstados AS value4, ot.Equipos_idEquipos AS value5,";
-            sql +=" ot.Servicios_idServicios AS value6, ot.Centro_idCentro AS value7, ot.Clientes_idClientes AS value8,";
+            sql +=" ot.idArea AS value3, area.Desc_Area AS value33,";
+            sql +=" ot.Estados_idEstados AS value4, estados.Desc_Status AS value44,";
+            sql +=" ot.Equipos_idEquipos AS value5, equipos.Desc_Equipo AS value55,";
+            sql +=" ot.Servicios_idServicios AS value6, servicios.Desc_Servicio AS value66,";
+            sql +=" ot.Centro_idCentro AS value7, centro.Centro_Costo AS value77,";
+            sql +=" ot.Clientes_idClientes AS value8, clientes.Nombres AS value88,";
             sql +=" ot.Fecha AS value9, ot.HES AS value10, ot.Valor AS value11, ot.Observacion AS value12  FROM ot"; 
             sql +=" INNER JOIN area on area.idArea=ot.idArea";
             sql +=" INNER JOIN centro on centro.idCentro=ot.Centro_idCentro";
