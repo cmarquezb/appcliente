2472bb92168976da8ea9aeb98370985d0d25ce33
Cristhian Márquez <cthan.mb@gmail.com>
2018-12-15 15:28:18 -0300

    Correcion consulta fecha

--- routes/ajax/ajaxMostrarOt.js
+++ routes/ajax/ajaxMostrarOt.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	req.getConnection(function(err,connection){
         
-        var cod_OT = req.query.term;              
+        var cod_OT = req.query.term;    
+        // formato fecha DATE_FORMAT(ot.Fecha, '%Y-%m-%d')   este no lo toma %D/%y/%a   
         //var sql = "SELECT idOT AS value, idOT AS label, idOT AS value1, idResponsables AS value2, idArea AS value3, Estados_idEstados AS value4, Equipos_idEquipos AS value5, Servicios_idServicios AS value6, Centro_idCentro AS value7, Clientes_idClientes AS value8, Fecha AS value9, HES AS value10, Valor AS value11, Observacion AS value12 FROM ot WHERE CAST(`idOT` AS CHAR)  LIKE '" +  cod_OT + "%' order by  idOT ASC";
         var sql = "SELECT ot.idOT AS value1, ot.idOT AS label,";
             sql +=" ot.idResponsables AS value2, responsables.Nombres AS value22,";
@@ -15,7 +16,7 @@ router.get('/', function(req, res, next) {
             sql +=" ot.Servicios_idServicios AS value6, servicios.Desc_Servicio AS value66,";
             sql +=" ot.Centro_idCentro AS value7, centro.Centro_Costo AS value77,";
             sql +=" ot.Clientes_idClientes AS value8, clientes.Nombres AS value88,";
-            sql +=" DATE_FORMAT(ot.Fecha, '%Y/%m/%d') AS value9, ot.HES AS value10, ot.Valor AS value11, ot.Observacion AS value12  FROM ot"; 
+            sql +=" DATE_FORMAT(ot.Fecha, '%Y-%m-%d') AS value9, ot.HES AS value10, ot.Valor AS value11, ot.Observacion AS value12  FROM ot"; 
             sql +=" INNER JOIN area on area.idArea=ot.idArea";
             sql +=" INNER JOIN centro on centro.idCentro=ot.Centro_idCentro";
             sql +=" INNER JOIN clientes on clientes.idClientes=ot.Clientes_idClientes";
